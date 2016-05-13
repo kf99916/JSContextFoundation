0e9dc2b14f1b7f882645203c970e394b79e69c07
Zheng-Xiang Ke <kf99916@gmail.com>
2016-05-14 00:13:58 +0800

    Initial Commit

---
+++ JSContextFoundation/JSContextFoundation.h
@@ -0,0 +1,19 @@
+//
+//  JSContextFoundation.h
+//  JSContextFoundation
+//
+//  Created by Zheng-Xiang Ke on 2016/5/14.
+//  Copyright © 2016年 Zheng-Xiang Ke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSContextFoundation.
+FOUNDATION_EXPORT double JSContextFoundationVersionNumber;
+
+//! Project version string for JSContextFoundation.
+FOUNDATION_EXPORT const unsigned char JSContextFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSContextFoundation/PublicHeader.h>
+
+
