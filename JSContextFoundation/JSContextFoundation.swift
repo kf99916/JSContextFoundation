d96fb4fc0c8fa3655601d58520a65e5f6e6c8078
Zheng-Xiang Ke <kf99916@gmail.com>
2016-05-14 11:55:36 +0800

    requireWithPath and requireWithUrl

--- JSContextFoundation/JSContextFoundation.swift
+++ JSContextFoundation/JSContextFoundation.swift
@@ -9,18 +9,71 @@
 import Foundation
 import JavaScriptCore
 
+public enum JSContextFoundationError: ErrorType {
+    case FileNotFound
+    case FileNotLoaded
+    case FileNotDownloaded
+}
+
 public class JSContextFoundation : JSContext {
     public override init!(virtualMachine: JSVirtualMachine!) {
         super.init(virtualMachine: virtualMachine)
         
-        let global = Global()
-        global.insert(self)
+        exceptionHandler = { context, exception in
+            print("[JSCotextFoundation][Exception] \(exception)")
+        }
         
-        let console = Console()
-        console.insert(self)
+        insert()
     }
     
     public convenience override init!() {
         self.init(virtualMachine: JSVirtualMachine())
+    }
+    
+    public func requireWithPath(path: String) throws {
+        guard NSFileManager.defaultManager().fileExistsAtPath(path) else {
+            throw JSContextFoundationError.FileNotFound
+        }
+        guard let script = try? String(contentsOfFile: path, encoding: NSUTF8StringEncoding) else {
+            throw JSContextFoundationError.FileNotLoaded
+        }
+        
+        evaluateScript(script)
+    }
+    
+    public func requireWithUrl(url: NSURL, completionHandler: (ErrorType?) -> Void) {
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            if let error = error {
+                completionHandler(error)
+            }
+            else {
+                guard let httpResponse = response as? NSHTTPURLResponse else {
+                    completionHandler(JSContextFoundationError.FileNotDownloaded)
+                    return
+                }
+                
+                switch httpResponse.statusCode {
+                case 404:
+                    completionHandler(JSContextFoundationError.FileNotFound)
+                default:
+                    guard let data = data, let script = String(data: data, encoding: NSUTF8StringEncoding) as String? else {
+                        completionHandler(JSContextFoundationError.FileNotDownloaded)
+                        return
+                    }
+                    
+                    self.evaluateScript(script)
+                    completionHandler(nil)
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func insert() {
+        let jsInsertArray: [JSInsert] = [Global(), Console()]
+        for jsInsert in jsInsertArray {
+            jsInsert.insert(self)
+        }
     }
 }
--- JSContextFoundationTests/JSContextFoundationTests.swift
+++ JSContextFoundationTests/JSContextFoundationTests.swift
@@ -23,15 +23,16 @@ class JSContextFoundationTests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        jsContext = nil
         super.tearDown()
     }
     
     func testVariablesAreDefined() {
-        jsContext!.evaluateScript("global")
-        jsContext!.evaluateScript("window")
-        jsContext!.evaluateScript("console")
-        jsContext!.evaluateScript("global.console")
-        jsContext!.evaluateScript("window.console")
+        jsContext!.evaluateScript("global;")
+        jsContext!.evaluateScript("window;")
+        jsContext!.evaluateScript("console;")
+        jsContext!.evaluateScript("global.console;")
+        jsContext!.evaluateScript("window.console;")
     }
     
     func testConsole() {
@@ -39,6 +40,59 @@ class JSContextFoundationTests: XCTestCase {
         jsContext!.evaluateScript("console.info('test console.info');")
         jsContext!.evaluateScript("console.warn('test console.warn');")
         jsContext!.evaluateScript("console.error('test console.error');")
+    }
+    
+    func testRequireWithPath() {
+        let path = NSBundle(forClass: self.dynamicType).pathForResource("MyClass", ofType: "js")!
+        
+        do {
+            try jsContext!.requireWithPath(path)
+        } catch JSContextFoundationError.FileNotFound {
+            XCTFail(path + " is not found")
+        } catch JSContextFoundationError.FileNotLoaded {
+            XCTFail(path + " cannot be loaded")
+        } catch {
+            XCTFail("Unknow Exception")
+        }
+        
+        jsContext!.evaluateScript(
+            "var myClass = new window.MyClass();" +
+            "window.console.log(myClass.value);"
+        )
+    }
+    
+    func testRequireWithUrl() {
+        guard let url = NSURL(string: "https://raw.githubusercontent.com/kf99916/JSContextFoundation/master/JSContextFoundationTests/MyClass.js") else {
+            XCTFail("Invalid Url")
+            return
+        }
+        
+        let asyncExpectation = expectationWithDescription("asyncExpectation")
+        
+        jsContext!.requireWithUrl(url, completionHandler: {error in
+            if let error = error {
+                switch error {
+                case JSContextFoundationError.FileNotFound:
+                    XCTFail(url.absoluteString + " is not found")
+                case JSContextFoundationError.FileNotDownloaded:
+                    XCTFail(url.absoluteString + " is not downloaded")
+                default:
+                    XCTFail("Unknown Error")
+                }
+                
+                return
+            }
+            
+            self.jsContext!.evaluateScript(
+                "var myClass = new window.MyClass();" +
+                "window.console.log(myClass.value);"
+            )
+            asyncExpectation.fulfill()
+        })
+        
+        waitForExpectationsWithTimeout(5) { error in
+            XCTAssertNil(error, "Timeout")
+        }
     }
     
     func testPerformanceExample() {
