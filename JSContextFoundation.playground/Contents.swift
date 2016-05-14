e9bf171bdd2dbec3714613fff0b800c9f05c7c62
Zheng-Xiang Ke <kf99916@gmail.com>
2016-05-14 11:54:53 +0800

    Remove the exceptionHandler in the playground to use the default

--- JSContextFoundation.playground/Contents.swift
+++ JSContextFoundation.playground/Contents.swift
@@ -3,9 +3,7 @@
 import JSContextFoundation
 
 let jsContext = JSContextFoundation()
-jsContext!.exceptionHandler = { context, exception in
-    print(exception)
-}
+
 jsContext.evaluateScript("console.log(1, 2, 3)")
 jsContext.evaluateScript("console.info(window)")
 jsContext.evaluateScript("console.error('a', {a: 3, b: 4}, [1, 2, 3])")
